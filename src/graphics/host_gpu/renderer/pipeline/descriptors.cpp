0a6be4e451b85ca13b1fcdaf49da7f53ff8ddda1
Lepexa-cell <emelianovdenis109@gmail.com>
2026-08-02 13:12:08 +0000

    V6

--- src/graphics/host_gpu/renderer/pipeline/descriptors.cpp
+++ src/graphics/host_gpu/renderer/pipeline/descriptors.cpp
@@ -391,7 +391,7 @@ static bool IsSupportedStorageTextureDescriptor(const ShaderRecompiler::IR::Imag
 	const bool supported_swizzle =
 	    IsValidImageSwizzle(swizzle) &&
 	    (swizzle == DstSel(4, 5, 6, 7) || !resource.read || resource.atomic);
-	const bool supported_mip_view = descriptor.BaseLevel() == 0 || is_1d || is_2d;
+	const bool supported_mip_view = descriptor.BaseLevel() == 0 || is_1d || is_1d_array || is_2d || is_2d_array || is_3d;;
 	return (is_1d || is_1d_array || is_2d || is_2d_array || is_3d) && supported_tile &&
 	       supported_mip_view && descriptor.BaseLevel() == descriptor.LastLevel() &&
 	       descriptor.LastLevel() <= descriptor.MaxMip() && descriptor.MinLod() == 0 &&
